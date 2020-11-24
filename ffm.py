a862ff2380463ead91995c9e87e186eee1d90ce1
Diwas Bhattarai <diwas@bhattarai.de>
2020-11-24 21:28:15 +0000

    still yuv cannot be shown correctly. brg can be send as an array or directly without encoding from server

---
+++ ffm.py
@@ -0,0 +1,104 @@
+import ffmpeg
+import subprocess
+import numpy as np
+import sys
+from scipy import ndimage
+
+class VideoCapture:
+    '''
+    Class for video capturing from video files, image sequences or cameras. 
+    '''
+    def __init__(self, source, pix_fmt="yuv420p"):
+        self.source = source
+        self.pix_fmt = pix_fmt
+        self.width, self.height = self.get_video_size() # or add custom values
+        # Note: RGB24 == 3 bytes per pixel.
+        self.setframeSize()
+        self.size = 0
+    
+    def setResolution(self, width, height):
+        self.width = width
+        self.height = height
+        self.setframeSize()
+    
+    def setframeSize(self):
+        if self.pix_fmt == "bgr24":
+            self.frame_size = self.width * self.height * 3
+        elif self.pix_fmt == "yuv420p":
+            Y = self.width * self.height
+            U = Y / 4
+            V = Y / 4
+            self.frame_size = int(Y + U + V)
+        else:
+            raise NotImplementedError()
+
+    def start(self):
+        self.process = self.start_ffmpeg_process()
+
+    def get_video_size(self):
+        probe = ffmpeg.probe(self.source)
+        video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
+        width = int(video_info['width'])
+        height = int(video_info['height'])
+        return width, height
+
+    def start_ffmpeg_process(self):
+        args = (
+            ffmpeg
+            .input(self.source, framerate=32)
+            .output('pipe:', 
+                    loglevel='quiet',
+                    format='rawvideo', 
+                    s=f'{self.width}x{self.height}', 
+                    pix_fmt=self.pix_fmt)
+            .compile()
+        )
+        return subprocess.Popen(args, stdout=subprocess.PIPE)
+
+    def read(self):
+        '''
+        Grabs, decodes and returns the next video frame. 
+        '''
+        in_bytes = self.grab()
+        frame = self.retrieve(in_bytes)
+        return frame
+
+    def grab(self):
+        '''
+        Grabs the next frame from video file or capturing device. 
+        '''
+        in_bytes = self.process.stdout.read(self.frame_size)
+        self.size = sys.getsizeof(in_bytes)
+        if self.size == 17:
+            raise Exception("Read 0 byte. No image was grabed.")
+        return in_bytes
+
+    def retrieve(self, in_bytes):
+        '''
+        Decodes and returns the grabbed video frame. 
+        '''
+        if len(in_bytes) == 0:
+            frame = None
+        else:
+            assert len(in_bytes) == self.frame_size
+        if self.pix_fmt == "bgr24":
+            frame = (
+                np
+                .frombuffer(in_bytes, np.uint8)
+                .reshape([self.height, self.width, 3])
+            )
+        elif self.pix_fmt == "yuv420p":
+                raise NotImplementedError()
+        else:
+            raise NotImplementedError()
+
+        return frame
+
+    def release(self):
+        self.process.kill()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.release()
---
+++ ffm_server.py
@@ -0,0 +1,50 @@
+import ffm
+from time import time, sleep
+import sys
+from zmq_tools import Msg_Streamer
+import zmq
+from payload import Payload
+import numpy as np
+import logging as logger
+import traceback
+#import cv2
+
+host = "10.3.141.1"
+port = "12349"
+ctx = zmq.Context()
+icp_req_add = "tcp://{}:{}".format(host, port)
+msg_streamer = Msg_Streamer(ctx, icp_req_add, hwm=1)
+
+source = "/dev/video0"
+cap = ffm.VideoCapture(source)
+
+width = 320
+height = 240
+frame = 1
+fps = 0
+index = 0
+start_time = time()
+cap.setResolution(width, height)
+cap.start()
+payload = Payload("world", width, height, format="yuv")
+image_read_time = time()
+try:
+    while True:
+        image = cap.grab()
+        latency = time() - image_read_time
+        payload.setPayloadParam(time(), image, index)
+        msg_streamer.send(payload.get())
+        if time() - start_time > 1:
+            fps = frame
+            frame = 0
+            start_time = time()
+        outstr = "Frames: {}, FPS: {}, Frame Read latency: {}".format(index, fps, latency)
+        sys.stdout.write('\r'+ outstr)
+        frame = frame + 1
+        index = index + 1
+        image_read_time = time()
+except Exception:
+    exp = traceback.format_exc()
+    print(exp)
+finally:
+    cap.release()
